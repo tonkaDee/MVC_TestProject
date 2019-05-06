2a5ba2e45002f5f299dd9295e71ac0590f72d37a
tonkaDee <50342655+tonkaDee@users.noreply.github.com>
2019-05-06 14:42:31 -0700

    InitialCommit
    TestProject Initial Commit

---
+++ MvcMovie-master/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HellowWorld/
+
+        public string Index()
+        {
+            return "This is my default action";
+        } // End Index()
+
+        //
+        // GET: /HellowWorld/Welcome/
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is : {numTimes} ") ;
+        }
+
+    }
+}
